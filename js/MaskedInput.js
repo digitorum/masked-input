40bcbb45b0c7694a065a6f95559841d38d59f3c3
Pavel Ladygin <me@digitorum.ru>
2017-01-30 12:22:01 +0300

    Прототип механизма наложения масок.

--- js/MaskedInput.js
+++ js/MaskedInput.js
@@ -1,19 +1,171 @@
 var MaskedInput = (function () {
     
+    //#region ParsedMask
+    
+    function ParsedMask(data) {
+        var mask = data.mask.split('');
+        var optional = data.optional ? data.optional : null;
+        
+        this.allow = data.templateAllow;
+        this.list = [];
+        for (var i = 0; i < mask.length; ++i) {
+            var letter = mask[i];
+            var node = {};
+            
+            if (letter == data.template) {
+                node.isAny = true;
+            } else if (optional && optional.indexOf(letter) !== -1) { // TODO: IE8
+                node.letter = letter;
+                node.isOptional = true;
+            } else { 
+                node.letter = letter;
+            }
+
+            this.list.push(node);
+        }
+    }
+    
+    /**
+     * Разрешенные символы в маске
+     * @var {rexp?}
+     */
+    ParsedMask.prototype.allow = null;
+
+    /**
+     * Разобранная маска
+     * @var {array}
+     */
+    ParsedMask.prototype.list = null;
+    
+    /**
+     * Заполнитьмаску
+     * @param text
+     * @return {object?}
+     */
+    ParsedMask.prototype.try = function (text) {
+        var result = '';
+        var optionals = [];
+        var iteration = 0;
+        
+        text = text.split('');
+        while (text.length) {
+            var letter = text.shift();
+            var matching = true;
+            
+            while (matching) {
+                var node = this.list[result.length];
+
+                if (!node) {
+                    // битая маска
+                    return null;
+                } else if (node.isOptional) {
+                    // опциональный символ
+                    result += node.letter;
+                    if (node.letter == letter) {
+                        // но если он совпал - дальше не ищем
+                        matching = false;
+                    } else {
+                        optionals.push({
+                            letter: node.letter,
+                            shift: iteration
+                        });
+                    }
+                } else if (node.isAny) {
+                    // "любой" символ
+                    if (this.allow.test(letter)) {
+                        result += letter;
+                        matching = false;
+                    } else {
+                        // ошибка заполенения маски: ошибочный символ
+                        return null;
+                    }
+                } else if (node.letter && node.letter == letter) {
+                    result += letter;
+                    matching = false;
+                } else {
+                    // ошибка заполнения маски
+                    return null;
+                }
+            }
+            iteration++;
+        }
+        return {
+            text: result,
+            optionals: optionals,
+            mask: this
+        };
+    }
+
+    //#endregion
+
+    //#region PhoneMask
+    
+    /**
+     * Класс, описывающий механизм работы с маской.
+     * По факту враппер над ParsedMask (для простоты прототипирования)
+     * @param maskData
+     * @constructor
+     */
+    function PhoneMask(maskData) {
+        this.data = maskData.data || {};
+        this.mask = new ParsedMask({
+            mask: maskData.mask,
+            template: '#',
+            templateAllow: /[0-9]/,
+            optional: ['+', ' ', '(', ')', '-']
+        });
+    }
+    
+    /**
+     * Данные приаттачяенные к маске
+     * @var {obkect?}
+     */
+    PhoneMask.prototype.data = null;
+    
+    /**
+     * Маска
+     * @var {ParsedMask}
+     */
+    PhoneMask.prototype.mask = null;
+    
+    /**
+     * Попытаться заполнить маску
+     * TODO: наследование
+     * @param text
+     * @return {object?}
+     */
+    PhoneMask.prototype.try = function (text) { 
+        return this.mask.try(text);
+    }
+    
+    //#endregion
+
     //#region MaskedInput
 
     /**
      * Инпут с маской
+     * @var options
      * @constructor
      */
-    function MaskedInput(domElement) {
-        this.domElement = domElement;
+    function MaskedInput(options) {
+        this.domElement = options.domElement;
         this.updateValue({
             action: this.actions.SET_TEXT,
             text: this.domElement.value
         });
+        if (options.masks) {
+            for (var i = 0; i < options.masks.length; ++i) { 
+                this.masks.push(new PhoneMask(options.masks[i]));
+            }
+        }
         this.bind();
     }
+    
+    /**
+     * Массив масок
+     * @var {array}
+     */
+    MaskedInput.prototype.masks = [];
 
     /**
      * Выполняемые действия
@@ -53,6 +205,12 @@ var MaskedInput = (function () {
      * @var {Object}
      */
     MaskedInput.prototype.attachedEvents = {};
+    
+    /**
+     * Данные текущей маски
+     * @var {Object?}
+     */
+    MaskedInput.prototype.currentMaskData = null;
 
     /**
      * Добавить обраотчики событий
@@ -127,7 +285,7 @@ var MaskedInput = (function () {
 
         this.updateValue({
             action: that.actions.CUT_TEXT
-        });
+        }, e);
     };
 
     /**
@@ -142,7 +300,7 @@ var MaskedInput = (function () {
             that.updateValue({
                 action: that.actions.INSERT_TEXT,
                 text: clipboardData.getData('Text') || ""
-            });
+            }, e);
         } else {
             e.preventDefault();
         }
@@ -156,7 +314,7 @@ var MaskedInput = (function () {
         this.updateValue({
             action: this.actions.SET_TEXT,
             text: this.domElement.value
-        });
+        }, e);
     };
 
     /**
@@ -169,10 +327,11 @@ var MaskedInput = (function () {
             return true;
         }
         // дописываем введенный символ в инпут
+        // TODO: обработать навигацию (нажитие клавиш влево-вправо)
         this.updateValue({
             action: this.actions.INSERT_TEXT,
             text: String.fromCharCode(e.charCode)
-        });
+        }, e);
     };
 
     /**
@@ -186,28 +345,19 @@ var MaskedInput = (function () {
             //  - заглавная буква будет доступна через {charCode} в Keypress, дополнительные проверки не нужны
             return true;
         }
-        if (e.keyCode == this.keyCodes.BACKSPACE) {
-            // удаление ПРЕДЫДУЩЕГО символа или селекшена
-            this.updateValue({
-                action: this.actions.DELETE_PREVIOUS
-            });
-        } else if (e.keyCode == this.keyCodes.DELETE) {
-            // удаление СЛЕДУЮЩЕГО символа или селекшена
-            this.updateValue({
-                action: this.actions.DELETE_NEXT
-            });
-        } else {
-            // проверка маски
-            // если нет подходящих масок, то прерываем ввод символа
-            var masks = this.findMasks(this.getNewValue({
-                action: this.actions.INSERT_TEXT,
-                text: String.fromCharCode(e.charCode)
-            }));
-            if (masks.length == 0) {
-                e.preventDefault();
-            } else {
-                // ищем и обарабатываем маску
-            }
+        switch (e.keyCode) {
+            case this.keyCodes.BACKSPACE:
+                // удаление ПРЕДЫДУЩЕГО символа или селекшена
+                this.updateValue({
+                    action: this.actions.DELETE_PREVIOUS
+                }, e);
+                break;
+            case this.keyCodes.DELETE:
+                // удаление СЛЕДУЮЩЕГО символа или селекшена
+                this.updateValue({
+                    action: this.actions.DELETE_NEXT
+                }, e);
+                break;
         }
     };
 
@@ -222,26 +372,51 @@ var MaskedInput = (function () {
     /**
      * Найти маску, подходящую под значение
      * @param text
-     * @return {Array}
+     * @return {string}
      */
-    MaskedInput.prototype.findMasks = function () {
-        return [null];
+    MaskedInput.prototype.findMaskValue = function (text) {
+        for (var i = 0; i < this.masks.length; ++i) {
+            var value = this.masks[i].try(text);
+
+            if (value) {
+                this.currentMaskData = value;
+                console.log(this.currentMaskData);
+                return value.text;
+            }
+        }
+        return '';
     }
-
+    
     /**
-     * Применить маску к инпуту
-     * @param mask
+     * Применить значение маски к инпуту
+     * @param value
      */
-    MaskedInput.prototype.applyMask = function (mask) {
-
+    MaskedInput.prototype.applyMaskValue = function (value) { 
+        this.domElement.value = value;
     }
 
     /**
      * Обновить значение
      * @param action
+     * @param e
      */
-    MaskedInput.prototype.updateValue = function (action) {
-        this.value = this.getNewValue(action);
+    MaskedInput.prototype.updateValue = function (action, e) {
+        var value = this.getNewValue(action);
+        
+        // TODO: маска должна понимать пустые значения
+        if (value) {
+            var maskValue = this.findMaskValue(value);
+            
+            if (e) {
+                e.preventDefault();
+            }
+            if (maskValue) {
+                this.applyMaskValue(maskValue);
+                this.value = value;
+            }
+        } else {
+            this.value = value;
+        }
     }
 
     /**
@@ -262,7 +437,7 @@ var MaskedInput = (function () {
                     value = this.value.slice(0, bound.start) + this.value.slice(bound.stop);
                     break;
                 case this.actions.INSERT_TEXT:
-                    value = this.value.slice(0, bound.start) + action.text + this.value.slice(bound.start);
+                    value = this.value.slice(0, bound.start) + action.text + this.value.slice(bound.stop);
                     break;
                 case this.actions.DELETE_PREVIOUS:
                     if (bound.start == 0 && bound.stop == 0) {
@@ -276,14 +451,10 @@ var MaskedInput = (function () {
                     }
                     break;
                 case this.actions.DELETE_NEXT:
-                    if (bound.start == 0 && bound.stop == 0) {
-                        value = this.value;
+                    if (bound.start == bound.stop) {
+                        value = this.value.slice(0, bound.start) + this.value.slice(bound.start + 1);
                     } else {
-                        if (bound.start == bound.stop) {
-                            value = this.value.slice(0, bound.start) + this.value.slice(bound.start + 1);
-                        } else {
-                            value = this.value.slice(0, bound.start) + this.value.slice(bound.stop);
-                        }
+                        value = this.value.slice(0, bound.start) + this.value.slice(bound.stop);
                     }
                     break;
             }
@@ -300,6 +471,36 @@ var MaskedInput = (function () {
      */
     MaskedInput.prototype.getSelectionBounds = function () {
         var start, stop;
+        var opts = this.currentMaskData ? this.currentMaskData.optionals : [];
+        
+        console.log(this.currentMaskData);
+
+        /**
+         * Посчитать количество опциональных символов которых нет в значении в определенном диапазоне
+         * @param optionals
+         * @param value
+         * @param shift
+         * @return {int}
+         */
+        function getoptionLettersCount(optionals, value, shift) {
+            var result = 0;
+            
+            if (value) {
+                value = value.split('');
+                for (var i = 0; i <= shift; ++i) {
+                    for (var j = 0; j < optionals.length; ++j) {
+                        var item = optionals[j];
+                        
+                        if (item.shift == i) {
+                            if (value[i] != item.letter) {
+                                result++;
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
 
         if (this.domElement.setSelectionRange) {
             start = this.domElement.selectionStart;
@@ -308,8 +509,8 @@ var MaskedInput = (function () {
             throw "not implemented";
         }
         return {
-            start: start,
-            stop: stop
+            start: start - getoptionLettersCount(opts, this.value, start),
+            stop: stop - getoptionLettersCount(opts, this.value, stop)
         };
     };
 
